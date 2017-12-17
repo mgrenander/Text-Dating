a122b87cb4919fb8bb4df1b21729e73fb7682c8a
mgrenander <matt.grenander@gmail.com>
2017-12-17 05:04:41 -0500

    Forgot to add files in last commit

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,19 +1,110 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+import math
+from shutil import copy2
+import numpy as np
+
+texts_folder = "Texts/"
+proc_folder = "Processed/"
+comb_folder = "Combined/"
+samples_folder = "Samples/"
 
 
-def parse():
+def parse(folder, filename):
     """Perform a variety of tasks to parse the raw text into usable format
-    1. Remove first and last sentences from each page of each book, and ignore first 10% and last 6%.
-    2. Remove punctuation and lower case every word"""
-    pass
+    1. Ignore first 10% and last 6% of the book, as per Bamman et al.
+    2. Remove first and last sentences from each page of each book.
+    """
+    # Check if the book has actually been downloaded
+    path = texts_folder + folder + "/" + filename
+    if len(os.listdir(path)) == 0:
+        print(filename + " has not been downloaded")
+        return
+
+    # We check if the file already has been parsed
+    if len(os.listdir(proc_folder + "/parsed.txt")) > 0:
+        print(filename + " has already been processed.")
+        return
+
+    # Compute cutoffs
+    print("Performing cutoff for book " + filename + " in folder " + folder)
+    numpages = len(os.listdir(path))
+    start_cut = int(math.ceil(0.1 * numpages))
+    end_cut = int(math.ceil(0.06 * numpages))
+
+    # Delete docs before start_cut and after end_cut
+    for i in range(start_cut, end_cut):
+        copy2(path + "/" + str(i) + '.txt', proc_folder + folder + "/" + filename)
+
+    # Remove first and last sentence from each document
+    for i in range(start_cut + 1, numpages - end_cut - 1):
+        with open(path + '/' + str(i) + '.txt') as fin:
+            page = fin.read().splitlines(True)
+        with open(path + '/' + str(i) + '.txt') as fout:
+            fout.writelines(page[1:])
+
+    # Put a marker saying we have parsed this document
+    f = open(path + "/parsed.txt", 'w')
+    f.close()
+
+
+def create_combined(folder):
+    """Combines all books from a time period into a single document in the 'Combined' directory"""
+    # Check if document already exists
+    if os.path.exists(comb_folder + folder + '/document.txt'):
+        print("Combined document already exists for " + folder)
+        return
+
+    # Open file and write all page from all books to it
+    f = open(comb_folder + folder + '/document.txt', 'a')
+    for book in os.listdir(proc_folder + folder):
+        for page in os.listdir(proc_folder + book):
+            f.write(page)
+    f.close()
+
 
 def create_vocabulary():
     """Process the words with TFIDF across the time domains and get top tfidf terms to make our vocabulary"""
+    vectorizer = TfidfVectorizer(input='content',
+                                 encoding='ignore',
+                                 strip_accents='unicode',
+                                 analyzer='word',
+                                 ngram_range=(1, 1),
+                                 token_pattern='(?u)\S\S+',
+                                 max_features=100000,
+                                 norm='l2',
+                                 smooth_idf=True)
+
+    # Create all the file paths
+    docs = []
+    for i in range(1750, 1925, 25):
+        foldername = comb_folder + "/{}-{}/document.txt".format(i, i + 25)
+        docs.append(foldername)
+
+    # Compute TFIDF
+    vectorizer.fit_transform(docs)
+
+    # Get top words
+    indices = np.argsort(vectorizer.idf_)[::-1]
+    features = vectorizer.get_feature_names()
+    top_features = [features[i] for i in indices[:10000]]
+
+    return top_features
+
+
+def create_samples(book_id, vocabulary):
+    """For a given book, create samples for this book, i.e. create the one-hot matrix encoding"""
     pass
 
-def get_raw_counts():
-    """For each term in the top TFIDF terms, get the raw counts"""
-    pass
+# ------------- Main script begins --------------- #
+for folder in os.listdir(texts_folder):
+    for bookid in os.listdir(texts_folder + '/' + folder):
+        parse(folder, bookid)
 
+    create_combined(folder)
 
-parse()
+vocabulary = create_vocabulary()
+
+for folder in os.listdir(texts_folder):
+    for bookid in folder:
+        create_samples(bookid, vocabulary)
--- downloader.py
+++ downloader.py
@@ -2,19 +2,37 @@ import hathitrust_api.data_api as da
 import os
 import sys
 import math
+import json
+import time
+from requests.exceptions import HTTPError, RequestException
 
 
-def create_folders():
+def create_folders(base):
     """Creates folders for the texts.
     Each folder is in the format YYYY-XXXX, where XXXX is YYYY+25 years"""
     for i in range(1750, 1925, 25):
-        foldername = "Texts/{}-{}".format(i, i + 25)
+        foldername = base + "/{}-{}".format(i, i + 25)
         if not os.path.exists(foldername):
             os.makedirs(foldername)
 
     # Create N/A category
-    if not os.path.exists("Texts/NA"):
-        os.makedirs("Texts/NA")
+    if not os.path.exists(base + "/NA"):
+        os.makedirs(base + "/NA")
+
+
+def getnumpages(data_api, id):
+    """Downloads meta and return the number of pages in the book"""
+    while True:
+        try:
+            meta = data_api.getmeta(id, json=True)
+            break
+        except HTTPError:
+            print("Request for meta for book failed. Trying again.")
+            time.sleep(5)
+            pass
+
+    meta_parsed = json.loads(meta)
+    return int(meta_parsed["htd:numpages"])
 
 
 def download_book(data_api, id, book_date_id):
@@ -27,26 +45,61 @@ def download_book(data_api, id, book_date_id):
     # Find folder to save text
     folder = "Texts/"
     if date == "N/A":
-        folder += "NA"
+        folder += "NA/" + str(id)
     else:
         rounded = math.floor(int(date) / 25) * 25
         folder += str(rounded) + "-" + str(rounded+25) + "/" + str(id)
 
-    # Download book
-    ocrtext = data_api.getdocumentocr(hathi_id)
-
-    # Save book to folder
+    # Create folder if we have not already
     if not os.path.exists(folder):
         os.makedirs(folder)
 
-    for i in range(0, len(ocrtext)):
+    # Get number of pages in the book
+    numpages = getnumpages(data_api, hathi_id)
+
+    # Check if we already downloaded all pages of this book
+    if os.path.exists(folder + "/{}.txt".format(numpages)) or os.path.exists(folder + "/parsed.txt"):
+        print("Already downloaded book " + str(id) + " in folder " + folder)
+        return
+
+    # Download book, page by page
+    print("Now downloading book " + str(id) + " in folder " + folder)
+    print("---> Pages downloaded (Total={}): ".format(numpages))
+    for i in range(1, numpages + 1):
         filename = folder + "/{}.txt".format(i)
+
+        # If file already exists we don't re-download
+        if os.path.exists(filename):
+            continue
+
+        attempts = 0
+        while True:
+            try:
+                ocrpage = data_api.getpageocr(hathi_id, i)
+                break
+            except (HTTPError, RequestException) as e:
+                # We attempt to download the page a max of 10 times, then exit with error if we are still failing
+                # Note there are lots of issues with the HathiTrust rejecting requests after a few page downloads
+                attempts += 1
+                time.sleep(5)  # Wait a few seconds before trying again
+                if attempts <= 100:
+                    print("Failed to download page " + str(i) + " with " + str(attempts) + " tries. Trying again.")
+                    pass
+                else:
+                    raise HTTPError("Failed to download book " + str(id) + " at page " + str(i)) from e
         f = open(filename, 'wb')
-        f.write(ocrtext[i])
+        f.write(ocrpage)
         f.close()
 
+        print(str(i), end=' ', flush=True)
+    print()
+
+# ------------- Main script begins --------------- #
 # Create folders if they have not been already
-create_folders()
+create_folders("Texts")
+create_folders("Processed")
+create_folders("Combined")
+create_folders("Samples")
 
 # Create data_api instance to create connection to HathiTrust
 oauth_key = sys.argv[1]
@@ -54,6 +107,9 @@ oauth_secret_key = sys.argv[2]
 data_api = da.DataAPI(oauth_key, oauth_secret_key)
 
 # Download the books according to Bamman et al. (2017)
-ids = open("stratified.txt").read().split("\n")[1:2]
+ids = open("stratified.txt").read().split("\n")[1:]
 for i in range(0, len(ids)):
+    if i == 19 or i == 31:
+        continue
+
     download_book(data_api, i, ids[i])
