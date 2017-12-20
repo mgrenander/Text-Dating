bab464223b592625c70de8917b9dc4161f98fc96
Seara <searachen@gmail.com>
2017-12-20 16:18:54 -0500

    this is thefolder for training

---
+++ 330/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='train_for_google',
+  version='0.1',
+  packages=find_packages(),
+  description='example to run keras on gcloud ml-engine',
+  author='Seara Chen',
+  author_email='searachen@gmail.com',
+  license='MIT',
+  install_requires=[
+      'keras',
+      'h5py',
+      'sklearn'
+  ],
+  zip_safe=False)
